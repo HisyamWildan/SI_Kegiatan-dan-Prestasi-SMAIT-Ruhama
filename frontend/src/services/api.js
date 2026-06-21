f8b6ef9e4abb01e594198f68b647a4267cea8e41
HisyamWildan <hisyamwildan79@gmail.com>
2026-06-21 20:57:10 +0700

    Keempat

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+export const API_BASE_URL = 'https://api.smaruhama.my.id';
 
 const api = axios.create({
-    baseURL: `${API_BASE_URL}/api`, // adjust to your backend URL
+    baseURL: API_BASE_URL, // adjust to your backend URL
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
